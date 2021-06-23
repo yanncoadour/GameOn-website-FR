39ca644b5ddd72f8d7d81d7d3dfa1fade6869354
COADOUR Yann <yanncoadour@gmail.com>
2021-06-23 13:05:37 +0200

    correction validationEmail

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -84,7 +84,7 @@ function validationNom() {
 
 //Email
 
-function valdiationEmail() {
+function validationEmail() {
   let email = document.getElementById("email");
   let erreurEmail = document.getElementById("error-email");
   let regexEmail = /^[a-z0-9.-_]+@[a-z0-9.-_]{2,}\.[a-z]{2,4}$/;
